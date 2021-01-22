81cc2d2f9515d05c00cf933f7e7bd331284c0458
Markus Läll <markus.l2ll@gmail.com>
2021-01-22 11:54:10 +0200

    Add Certificate signing, make it a library + executable

---
+++ src/X509/Certificate.hs
@@ -0,0 +1,180 @@
+module X509.Certificate where
+
+import Data.ASN1.Types
+import Data.List
+import Control.Monad.IO.Class
+import Control.Monad.Except
+
+import Time.System as Time
+import Time.Types as Time
+
+import Data.X509 hiding (Certificate) -- fixme
+import qualified Data.X509.PKCS10 as PKCS10
+import qualified Data.X509 as X509
+
+-- hs-certificate tests Certificate.hs
+import qualified Crypto.PubKey.DSA        as DSA
+import qualified Crypto.PubKey.ECC.ECDSA  as ECDSA
+import qualified Crypto.PubKey.Ed25519    as Ed25519
+import qualified Crypto.PubKey.Ed448      as Ed448
+import qualified Crypto.PubKey.RSA        as RSA
+import qualified Crypto.PubKey.RSA.PKCS15 as RSA
+import qualified Crypto.PubKey.RSA.PSS    as PSS
+
+import qualified Data.ByteString as B
+import Data.Maybe
+import Data.String
+import Data.ASN1.Encoding
+import Data.ByteArray (convert)
+import Data.ASN1.BinaryEncoding (DER(..))
+
+<<<<<<< HEAD:src/Certificate.hs
+import qualified Certificate.SignatureAlgorithm as SA
+import Certificate.SignatureAlgorithm (SignatureAlgorithm, hashAlgorithm)
+=======
+import qualified X509.SignatureAlgorithm as SA
+import X509.SignatureAlgorithm (SignatureAlgorithm, hashAlgorithm)
+import qualified Key
+>>>>>>> f9cd529 (fixup! Add Certificate signing, make it a library + executable):src/X509/Certificate.hs
+
+signCsr
+  :: (Monad m, MonadIO m)
+  => PKCS10.SignedCertificationRequest
+  -> RSA.PrivateKey -> ASN1CharacterString
+  -> ExceptT String m
+  (SignatureAlgorithm Key.RSA, X509.SignedCertificate)
+signCsr csr caPriv issuerName = do
+  commonName :: String <- maybe (throwError "") return $ do
+    let PKCS10.X520Attributes xs = PKCS10.subject $ PKCS10.certificationRequestInfo $ PKCS10.certificationRequest csr :: PKCS10.X520Attributes
+    (_, cn) <- find ((PKCS10.X520CommonName ==) . fst) xs
+    asn1CharacterToString cn
+
+  now <- liftIO $ Time.dateCurrent
+
+  let
+    issuerDN = mkCN issuerName
+    date = Time.dtDate now
+    date' = date { Time.dateYear = (Time.dateYear date + 1) }
+    until = now { Time.dtDate = date' }
+    validity = (now, until) :: (DateTime, DateTime)
+
+    pubKey = PKCS10.subjectPublicKeyInfo $ PKCS10.certificationRequestInfo $ PKCS10.certificationRequest csr
+
+    -- extensions :: Extensions
+    -- extensions = Extensions $ Just $
+    --   -- extensionEncode True $ ExtBasicConstraints True (Just 0)
+    --   [ ExtensionRaw {extRawOID = [2,5,29,19], extRawCritical = True, extRawContent = "0\NUL" }
+    --   , extensionEncode True $ ExtKeyUsage [KeyUsage_digitalSignature, KeyUsage_nonRepudiation, KeyUsage_keyEncipherment, KeyUsage_dataEncipherment]
+    --   -- , ExtensionRaw {extRawOID = [2,5,29,15], extRawCritical = True, extRawContent = "\ETX\STX\EOT\240"}
+    --   , extensionEncode True $ ExtExtendedKeyUsage [KeyUsagePurpose_ClientAuth]
+    --   ]
+
+    sigAlg' = SA.RSA 0 SA.hashSHA256 :: SA.SignatureAlgorithm RSA.PublicKey RSA.PrivateKey
+
+  liftIO $ mkLeaf commonName validity (caPriv, sigAlg', issuerDN) pubKey
+
+-- | Sign @message@ with a signature algorithm and a fitting private key
+sign :: SignatureAlgorithm pub priv -> priv -> B.ByteString -> IO (Either RSA.Error B.ByteString)
+sign sa key message = case sa of
+  SA.RSA _ hash -> RSA.signSafer (Just $ hashAlgorithm hash) key message
+  SA.RSAPSS _ params _ -> PSS.signSafer params key message
+  SA.DSA _ hash -> do
+    sig <- DSA.sign key (hashAlgorithm hash) message
+    return $ Right $ encodeASN1' DER
+      [ Start Sequence
+      , IntVal (DSA.sign_r sig)
+      , IntVal (DSA.sign_s sig)
+      , End Sequence
+      ]
+  SA.EC _ hash -> do
+    sig <- ECDSA.sign key (hashAlgorithm hash) message
+    return $ Right $ encodeASN1' DER
+      [ Start Sequence
+      , IntVal (ECDSA.sign_r sig)
+      , IntVal (ECDSA.sign_s sig)
+      , End Sequence
+      ]
+  SA.Ed25519 -> return $ Right $ convert $ Ed25519.sign key (Ed25519.toPublic key) message
+  SA.Ed448 -> return $ Right $ convert $ Ed448.sign key (Ed448.toPublic key) message
+
+-- * New system
+
+type Authority pub priv = (priv, SignatureAlgorithm pub priv, DistinguishedName)
+
+-- | Builds a certificate using the supplied keys and signs it with an
+-- authority (itself or another certificate).
+mkCertificate
+  :: forall pubI privI.
+     Int                        -- ^ Certificate version
+  -> Integer                    -- ^ Serial number
+  -> DistinguishedName          -- ^ Subject DN
+  -> (DateTime, DateTime)       -- ^ Certificate validity period
+  -> [ExtensionRaw]             -- ^ Extensions to include
+  -> Authority pubI privI           -- ^ Authority signing the new certificate
+  -> PubKey                     -- ^ Keys for the new certificate
+  -> IO (SignatureAlgorithm pubI privI, SignedCertificate)       -- ^ The new certificate/key pair
+mkCertificate version serial dn validity exts (signingKey, algI, issuerDN) tbsPub = let
+
+    signAlgI = SA.signatureALG algI :: SignatureALG
+    extensions = Extensions (if null exts then Nothing else Just exts) :: Extensions
+
+    signatureFunction :: B.ByteString -> IO (B.ByteString, SignatureALG)
+    signatureFunction objRaw = do
+      sigBits <- either (error . show) return =<< sign algI signingKey objRaw
+      return (sigBits, signAlgI)
+
+    tbs = X509.Certificate
+      { certVersion      = version
+      , certSerial       = serial
+      , certSignatureAlg = signAlgI
+      , certIssuerDN     = issuerDN
+      , certValidity     = validity
+      , certSubjectDN    = dn
+      , certPubKey       = tbsPub
+      , certExtensions   = extensions
+      }
+
+  in do
+    signedCert :: SignedCertificate <- objectToSignedExactF signatureFunction tbs
+    return (algI, signedCert)
+
+mkCA
+  :: Integer                    -- ^ Serial number
+  -> String                     -- ^ Common name
+  -> (DateTime, DateTime)       -- ^ Validity period
+  -> Maybe ExtBasicConstraints  -- ^ Basic constraints
+  -> Maybe ExtKeyUsage          -- ^ Key usage
+  -> Authority pubI privI           -- ^ Authority. CA is self-signed, so authority cryptosystem matches to-be-signed's
+  -> pubI                       -- ^ Public key of the certificate
+  -> IO (SignatureAlgorithm pubI privI, SignedCertificate)
+mkCA serial cn validity bc ku auth@ (_, sig, _) pub = let
+    exts = catMaybes [ mkExtension True <$> bc, mkExtension False <$> ku ]
+    pub' = SA.getPubKey sig pub
+  in mkCertificate 2 serial (mkCN $ fromString cn) validity exts auth pub'
+
+mkLeaf
+  :: String               -- ^ Common name
+  -> (DateTime, DateTime) -- ^ Certificate validity period
+  -> Authority pubI privI     -- ^ Authority signing the new certificate
+  -> PubKey
+  -> IO (SignatureAlgorithm pubI privI, SignedCertificate)       -- ^ The new leaf certificate/key pair
+mkLeaf cn validity auth pub = mkCertificate 2 100 (mkCN $ fromString cn) validity leafStdExts auth pub
+  where
+    -- | Default extensions in leaf certificates.
+    leafStdExts :: [ExtensionRaw]
+    leafStdExts = [ku, eku]
+      where
+        ku  = mkExtension False $ ExtKeyUsage
+                   [ KeyUsage_digitalSignature , KeyUsage_keyEncipherment ]
+        eku = mkExtension False $ ExtExtendedKeyUsage
+                   [ KeyUsagePurpose_ServerAuth , KeyUsagePurpose_ClientAuth ]
+
+-- * Helpers
+
+-- | Builds a DN with a single component.
+mkCN :: ASN1CharacterString -> DistinguishedName
+mkCN cn = DistinguishedName [(getObjectID DnCommonName, cn)]
+
+-- | Used to build a certificate extension.
+mkExtension :: Extension a => Bool -> a -> ExtensionRaw
+mkExtension crit ext = ExtensionRaw (extOID ext) crit (extEncodeBs ext)
---
+++ src/X509/SignatureAlgorithm.hs
@@ -0,0 +1,80 @@
+module X509.SignatureAlgorithm where
+
+import qualified Data.ByteString as BS
+
+import Data.X509 (HashALG(..), SignatureALG(..), PubKeyALG(..), PubKey(..), PubKeyEC(..), SerializedPoint(..))
+
+import Crypto.Number.Serialize (i2ospOf_)
+import Crypto.Hash.Algorithms
+import qualified Crypto.PubKey.DSA        as DSA
+import qualified Crypto.PubKey.ECC.ECDSA  as ECDSA
+import qualified Crypto.PubKey.ECC.Types  as ECC
+import qualified Crypto.PubKey.Ed25519    as Ed25519
+import qualified Crypto.PubKey.Ed448      as Ed448
+import qualified Crypto.PubKey.RSA        as RSA
+import qualified Crypto.PubKey.RSA.PKCS15 as RSA
+import qualified Crypto.PubKey.RSA.PSS    as PSS
+
+-- * Match @hash@ algorithm type to @HashALG@ value
+
+data Hash hash = Hash
+  { hashALG :: HashALG      -- ADT from Data.X509.AlgorithmIdentifier
+  , hashAlgorithm :: hash } -- Each its own type
+
+hashMD2 :: Hash MD2
+hashMD2 = Hash HashMD2 MD2
+
+hashMD5 :: Hash MD5
+hashMD5 = Hash HashMD5 MD5
+
+hashSHA1 :: Hash SHA1
+hashSHA1 = Hash HashSHA1 SHA1
+
+hashSHA224 :: Hash SHA224
+hashSHA224 = Hash HashSHA224 SHA224
+
+hashSHA256 :: Hash SHA256
+hashSHA256 = Hash HashSHA256 SHA256
+
+hashSHA384 :: Hash SHA384
+hashSHA384 = Hash HashSHA384 SHA384
+
+hashSHA512 :: Hash SHA512
+hashSHA512 = Hash HashSHA512 SHA512
+
+-- * Signature algorithm
+
+-- | Signature algorithm consists of a hash algorithm plus a public
+-- key cryptosystem to sign the hash. (The cryptosystem doesn't
+-- determine the signing algorithm as for RSA there are two.)
+data SignatureAlgorithm pub priv where
+  RSA :: (HashAlgorithm hash, RSA.HashAlgorithmASN1 hash) => Int -> Hash hash -> SignatureAlgorithm RSA.PublicKey RSA.PrivateKey
+  RSAPSS :: HashAlgorithm hash => Int -> PSS.PSSParams hash BS.ByteString BS.ByteString -> Hash hash -> SignatureAlgorithm RSA.PublicKey RSA.PrivateKey
+  DSA :: HashAlgorithm hash => DSA.Params -> Hash hash -> SignatureAlgorithm DSA.PublicKey DSA.PrivateKey
+  EC :: HashAlgorithm hash => ECC.CurveName -> Hash hash -> SignatureAlgorithm ECDSA.PublicKey ECDSA.PrivateKey
+  Ed25519 :: SignatureAlgorithm Ed25519.PublicKey Ed25519.SecretKey
+  Ed448 :: SignatureAlgorithm Ed448.PublicKey Ed448.SecretKey
+
+signatureALG :: SignatureAlgorithm pub priv -> SignatureALG
+signatureALG sa = case sa of
+  RSA _ hash -> SignatureALG (hashALG hash) PubKeyALG_RSA
+  RSAPSS _ _ hash -> SignatureALG (hashALG hash) PubKeyALG_RSAPSS
+  DSA _ hash -> SignatureALG (hashALG hash) PubKeyALG_DSA
+  EC _ hash -> SignatureALG (hashALG hash) PubKeyALG_EC
+  Ed25519 -> SignatureALG_IntrinsicHash PubKeyALG_Ed25519
+  Ed448 -> SignatureALG_IntrinsicHash PubKeyALG_Ed448
+
+getPubKey :: SignatureAlgorithm pub priv -> pub -> PubKey
+getPubKey sa key = case sa of
+  RSA _ _ -> PubKeyRSA key
+  RSAPSS _ _ _ -> PubKeyRSA key
+  DSA _ _ -> PubKeyDSA key
+  EC name _ -> PubKeyEC (PubKeyEC_Named name pub)
+    where
+      ECC.Point x y = ECDSA.public_q key
+      pub   = SerializedPoint bs
+      bs    = BS.cons 4 (i2ospOf_ bytes x `BS.append` i2ospOf_ bytes y)
+      bits  = ECC.curveSizeBits (ECC.getCurveByName name)
+      bytes = (bits + 7) `div` 8
+  Ed25519 -> PubKeyEd25519 key
+  Ed448 -> PubKeyEd448 key
