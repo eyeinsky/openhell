7594be3561c310af6dc6751002301ab0d9ae49b2
Markus Läll <markus.l2ll@gmail.com>
2021-01-26 17:47:17 +0200

    Refactor certificate making
    - get rid of the `Authority alg` type
    - pass a partial certificate (the "to be signed" certificate, really)
      into the mkCertificate/mkLeaf/mkCA function;
    - also set extensions beforehand

--- src/X509/Certificate.hs
+++ src/X509/Certificate.hs
@@ -32,91 +32,16 @@ signCsr csr caPriv issuerName = do
   now <- liftIO $ Time.dateCurrent
 
   let
-    issuerDN = mkCN issuerName
     date = Time.dtDate now
     date' = date { Time.dateYear = (Time.dateYear date + 1) }
     until = now { Time.dtDate = date' }
     validity = (now, until) :: (DateTime, DateTime)
 
+    pubKey :: X509.PubKey
     pubKey = PKCS10.subjectPublicKeyInfo $ PKCS10.certificationRequestInfo $ PKCS10.certificationRequest csr
-
-    -- extensions :: Extensions
-    -- extensions = Extensions $ Just $
-    --   -- extensionEncode True $ ExtBasicConstraints True (Just 0)
-    --   [ ExtensionRaw {extRawOID = [2,5,29,19], extRawCritical = True, extRawContent = "0\NUL" }
-    --   , extensionEncode True $ ExtKeyUsage [KeyUsage_digitalSignature, KeyUsage_nonRepudiation, KeyUsage_keyEncipherment, KeyUsage_dataEncipherment]
-    --   -- , ExtensionRaw {extRawOID = [2,5,29,15], extRawCritical = True, extRawContent = "\ETX\STX\EOT\240"}
-    --   , extensionEncode True $ ExtExtendedKeyUsage [KeyUsagePurpose_ClientAuth]
-    --   ]
 
     sigAlg' = Signature.RSA Signature.hashSHA256 :: Signature.Algorithm Key.RSA
 
-  liftIO $ mkLeaf commonName validity (caPriv, sigAlg', issuerDN) pubKey
-
--- * New system
-
-type Authority alg = (Key.Private alg, Signature.Algorithm alg, DistinguishedName)
-
--- | Builds a certificate using the supplied keys and signs it with an
--- authority (itself or another certificate).
-mkCertificate
-  :: forall alg.
-     Int                        -- ^ Certificate version
-  -> Integer                    -- ^ Serial number
-  -> DistinguishedName          -- ^ Subject DN
-  -> (DateTime, DateTime)       -- ^ Certificate validity period
-  -> [ExtensionRaw]             -- ^ Extensions to include
-  -> Authority alg           -- ^ Authority signing the new certificate
-  -> PubKey                     -- ^ Keys for the new certificate
-  -> IO (Signature.Algorithm alg, SignedCertificate)       -- ^ The new certificate/key pair
-mkCertificate version serial dn validity exts (signingKey, algI, issuerDN) tbsPub = let
-
-    signAlgI = Signature.signatureALG algI :: SignatureALG
-    extensions = Extensions (if null exts then Nothing else Just exts) :: Extensions
-
-    signatureFunction :: BS.ByteString -> IO (BS.ByteString, SignatureALG)
-    signatureFunction objRaw = do
-      sigBits <- either (error . show) return =<< Signature.sign algI signingKey objRaw
-      return (sigBits, signAlgI)
-
-    tbs = X509.Certificate
-      { certVersion      = version
-      , certSerial       = serial
-      , certSignatureAlg = signAlgI
-      , certIssuerDN     = issuerDN
-      , certValidity     = validity
-      , certSubjectDN    = dn
-      , certPubKey       = tbsPub
-      , certExtensions   = extensions
-      }
-
-  in do
-    signedCert :: SignedCertificate <- objectToSignedExactF signatureFunction tbs
-    return (algI, signedCert)
-
-mkCA
-  :: (Key.ToPubKey (Key.Public alg))
-  => Integer                    -- ^ Serial number
-  -> String                     -- ^ Common name
-  -> (DateTime, DateTime)       -- ^ Validity period
-  -> Maybe ExtBasicConstraints  -- ^ Basic constraints
-  -> Maybe ExtKeyUsage          -- ^ Key usage
-  -> Authority alg           -- ^ Authority. CA is self-signed, so authority cryptosystem matches to-be-signed's
-  -> Key.Public alg                       -- ^ Public key of the certificate
-  -> IO (Signature.Algorithm alg, SignedCertificate)
-mkCA serial cn validity bc ku auth@ (_, sig, _) pub = let
-    exts = catMaybes [ mkExtension True <$> bc, mkExtension False <$> ku ]
-    pub' = Key.toPubKey pub
-  in mkCertificate 2 serial (mkCN $ fromString cn) validity exts auth pub'
-
-mkLeaf
-  :: String               -- ^ Common name
-  -> (DateTime, DateTime) -- ^ Certificate validity period
-  -> Authority alg     -- ^ Authority signing the new certificate
-  -> PubKey
-  -> IO (Signature.Algorithm alg, SignedCertificate)       -- ^ The new leaf certificate/key pair
-mkLeaf cn validity auth pub = mkCertificate 2 100 (mkCN $ fromString cn) validity leafStdExts auth pub
-  where
     -- | Default extensions in leaf certificates.
     leafStdExts :: [ExtensionRaw]
     leafStdExts = [ku, eku]
@@ -125,6 +50,58 @@ mkLeaf cn validity auth pub = mkCertificate 2 100 (mkCN $ fromString cn) validit
                    [ KeyUsage_digitalSignature , KeyUsage_keyEncipherment ]
         eku = mkExtension False $ ExtExtendedKeyUsage
                    [ KeyUsagePurpose_ServerAuth , KeyUsagePurpose_ClientAuth ]
+
+    tbs :: TBS
+    tbs = X509.Certificate
+      { certVersion = 2
+      , certSerial = 100
+      , certValidity = validity
+
+      , certSubjectDN = mkCN $ fromString commonName
+      , certIssuerDN = mkCN issuerName
+
+      , certExtensions = Extensions $ if null leafStdExts then Nothing else Just leafStdExts
+      }
+
+  liftIO $ mkLeaf tbs caPriv sigAlg' pubKey
+
+
+type TBS = X509.Certificate
+
+mkCertificate
+  :: TBS
+  -> Key.Private alg -> Signature.Algorithm alg
+  -> PubKey
+  -> IO (Signature.Algorithm alg, SignedCertificate)       -- ^ The new certificate/key pair
+mkCertificate tbs signingKey algI tbsPub = let
+    signAlgI = Signature.signatureALG algI :: SignatureALG
+    signatureFunction :: BS.ByteString -> IO (BS.ByteString, SignatureALG)
+    signatureFunction objRaw = do
+      sigBits <- either (error . show) return =<< Signature.sign algI signingKey objRaw
+      return (sigBits, signAlgI)
+
+    tbs' = tbs
+      { certSignatureAlg = signAlgI
+      , certPubKey       = tbsPub
+      }
+  in do
+    signedCert :: SignedCertificate <- objectToSignedExactF signatureFunction tbs'
+    return (algI, signedCert)
+
+mkCA
+  :: (Key.ToPubKey (Key.Public alg))
+  => TBS
+  -> Key.Private alg -> Signature.Algorithm alg -- authority
+  -> Key.Public alg
+  -> IO (Signature.Algorithm alg, SignedCertificate)
+mkCA tbs priv sigAlg pub = mkCertificate tbs priv sigAlg (Key.toPubKey pub)
+
+mkLeaf
+  :: TBS
+  -> Key.Private alg -> Signature.Algorithm alg -- authority
+  -> PubKey
+  -> IO (Signature.Algorithm alg, SignedCertificate)
+mkLeaf tbs priv sigAlg pub = mkCertificate tbs priv sigAlg pub
 
 -- * Helpers
 
